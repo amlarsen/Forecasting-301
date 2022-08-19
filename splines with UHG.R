e681b7bb8e64d97126b247d5bb5dd20b1a67b512
amlarsen <40773600+amlarsen@users.noreply.github.com>
2022-08-19 09:17:28 -0500

    Update splines with UHG.R

--- splines with UHG.R
+++ splines with UHG.R
@@ -1,14 +1,11 @@
+#import packages
 library(fpp2)
 library(readr)
 library(splines)
 
+#import data
 uheps <- read_csv("R/United Health EPS.csv", col_types = cols(EPS = col_number()))
 View(uheps)
-
-fit_1 <- lm(EPS ~ Order, data=uheps)
-summary(fit_1)
-
-predict(fit_1, data.frame(Order=59))
 
 #splines
 uhts <- ts(uheps[,2],start = c(2005,1),frequency = 4)
